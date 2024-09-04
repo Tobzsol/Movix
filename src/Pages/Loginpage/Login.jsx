c246ec01fa4c8c51d51a774478ab80c84116423e
Tobzsolo <oluwatobasolo@gmail.com>
2024-09-04 22:10:15 +0100

    first commit

--- src/Pages/Loginpage/Login.jsx
+++ src/Pages/Loginpage/Login.jsx
@@ -4,13 +4,12 @@ import React from 'react';
 function Login(props) {
     return (
         <div className='login-details'>
-            <img src="/public/Image/Logo.png" alt="" />
             <h1>Hi, Welcome</h1>
             <p>Please sign-up to start your experience</p>
             <div className='login-input'>
             <input type="email" placeholder='Email' />
             <input type="password" placeholder='Password' />
-            <button className='login-button' type="submit">Login</button>
+            <button className='login-button' type="submit">LOGIN</button>
             </div>
             <div className='login-alternative'>
             <p>Don't have an account?</p>
